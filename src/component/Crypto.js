d547d687150dae4427e7d49ed5234d0738acfd39
Alwaleed Fares <110339523+waleed00439@users.noreply.github.com>
2022-09-08 13:19:07 +0300

    Add files via upload

---
+++ src/component/Crypto.js
@@ -0,0 +1,14 @@
+// import React from "react";
+// import Select from 'react-select';
+
+
+
+// const Crypto = ({ countryList, onSelect }) => {
+//     return (
+//       <div className='input-group mt-3 w-50 mx-auto'>
+//         <Select onChange={onSelect} className='w-100' options={countryList} />
+//       </div>
+//     );
+//   };
+  
+//   export default Crypto;
